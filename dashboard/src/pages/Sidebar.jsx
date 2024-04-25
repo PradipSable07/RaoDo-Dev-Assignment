12809e1f9da6cb52713ea343fe38b22cb0901e80
Pradip Sable <sablepradipsubhash@gmail.com>
2024-04-25 17:47:37 +0530

    added side bar

--- dashboard/src/pages/Sidebar.jsx
+++ dashboard/src/pages/Sidebar.jsx
@@ -1,57 +1,9 @@
-import React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import Collapse from   '@mui/material/Collapse';
+import React from 'react'
 
 const Sidebar = () => {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
   return (
-    <div style={{ width: 250, padding: '10px' }}>
-      <List>
-        <ListItem button>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary="Dashboard" />
-        </ListItem>
-        <ListItem button onClick={handleClick}>
-          <ListItemIcon>
-            <MailIcon />
-          </ListItemIcon>
-          <ListItemText primary="Orders" />
-          {open ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
-        <Collapse in={open} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItem button>
-              <ListItemIcon />
-              <ListItemText primary="All Orders" />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon />
-              <ListItemText primary="Pending Orders" />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon />
-              <ListItemText primary="Completed Orders" />
-            </ListItem>
-          </List>
-        </Collapse>
+    <div>Sidebar</div>
+  )
+}
 
-      </List>
-    </div>
-  );
-};
-
-export default Sidebar;
+export default Sidebar
